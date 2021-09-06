9b22452d85f8e9476a8b3a9911f790509887ab27
Santiago <santiagojavierveiga@gmail.com>
2021-09-06 18:36:02 -0300

    feat(client/Home): added Home view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Login} />
+      <Route exact path="/home" component={Home} />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { SideBar } from '../SideBar/SideBar';
+
+export function Home() {
+  return (
+    <div>
+      <div>Esto es el Home</div>
+      <SideBar />
+    </div>
+  );
+}
